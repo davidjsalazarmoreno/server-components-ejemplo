ca55095b9dab4518cbfe62ce4f47bea0c91eece5
David Salazar <davidjsalazarmoreno@gmail.com>
2023-05-29 11:00:50 -0500

    feat: :sparkles: adds comments api

---
+++ app/api/comments/route.ts
@@ -0,0 +1,40 @@
+import { NextResponse } from "next/server";
+
+const ENDPOINT = "https://hacker-news.firebaseio.com/v0";
+
+// https://github.com/HackerNews/API#items
+export interface HackerNewsItem {
+  by: string;
+  descendants: number;
+  id: number;
+  kids: number[];
+  score: number;
+  time: number;
+  title: string;
+  type: string;
+  url: string;
+  text: string;
+}
+
+async function getItemFromId(id: number | string): Promise<HackerNewsItem> {
+  const url = `${ENDPOINT}/item/${id}.json`;
+
+  console.log(`Fetching: ${url}`);
+  try {
+    const response = await fetch(url);
+    const article: HackerNewsItem = await response.json();
+    return article;
+  } catch (error) {
+    console.log(error);
+    return {} as HackerNewsItem;
+  }
+}
+
+export async function GET(request: Request) {
+  const params = new URL(request.url).searchParams;
+  const ids = params.get("ids")?.split(",") ?? [];
+  const comments = await Promise.all(ids.map(getItemFromId));
+
+  console.log(ids);
+  return NextResponse.json(comments);
+}
